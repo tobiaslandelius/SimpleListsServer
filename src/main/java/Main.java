f4207a5b4d1284a77b02c68c74865a08fb40e28a
tobiaslandelius <tlandelius@gmail.com>
2017-06-18 09:33:45 +0200

    Kebab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! System up and running asdasdasdasd!");
+        System.out.println("Hello, World! System up and running kebab!");
         throw new Exception();
     }
 }
