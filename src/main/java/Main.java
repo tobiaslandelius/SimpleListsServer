94e22ddfefee081d330ecda0e981ce9009b4793a
tobiaslandelius <tlandelius@gmail.com>
2017-06-18 12:10:09 +0200

    Removed thrown exception

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,5 @@ public class Main {
      */
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! System up and running kebab!");
-        throw new Exception();
     }
 }
